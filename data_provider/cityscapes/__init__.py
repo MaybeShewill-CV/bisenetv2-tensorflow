4ae4f71ccd953df540584b47eaa098f5d5a60718
luoyao <luoyao@baidu.com>
2020-06-19 17:30:08 +0800

    update

--- data_provider/cityscapes/__init__.py
+++ data_provider/cityscapes/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Time    : 2020/6/18 下午2:41
-# @Author  : LuoYao
-# @Site    : ICode
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/bisenetv2-tensorflow
 # @File    : __init__.py.py
 # @IDE: PyCharm
